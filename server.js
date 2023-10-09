c3336d4faa28f74680b5fb895a942d3bc4001559
nhy42 <26034461+nhy42@users.noreply.github.com>
2023-10-09 15:21:38 +0200

    Basic Express server
    + homepage

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mysql = require("mysql");  // for later use
+const cookieParser = require("cookie-parser");  // for later use
+const app = express();
+const port = 80;
+
+app.set("view engine", "ejs")
+app.use(cookieParser());
+app.use("/css", express.static(__dirname + "/css"));
+app.use("/js", express.static(__dirname + "/js"));
+app.use("/img", express.static(__dirname + "/img"));
+
+app.get("/", (req, res) => {
+    res.render(__dirname + "/private/template/home.ejs");
+});
+
+app.get("/products", (req, res) => {
+    res.render(__dirname + "/private/template/products.ejs");
+});
+
+app.get("/reviews", (req, res) => {
+    res.render(__dirname + "/private/template/reviews.ejs");
+});
+
+app.get("/contact", (req, res) => {
+    res.render(__dirname + "/private/template/contact.ejs");
+});
+
+app.get("/about", (req, res) => {
+    res.render(__dirname + "/private/template/about.ejs");
+});
+
+
+// server start
+app.listen(port, () => {
+    console.log(`Cosmetic server started. Container port : ${port}`);
+});
