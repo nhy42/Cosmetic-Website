369c8a79dcf001cdae74327d71defb33c8f4d248
nhy42 <26034461+nhy42@users.noreply.github.com>
2023-11-13 16:28:00 +0100

    db.js (stolen)

---
+++ utils/db.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
+});
+
+module.exports = {
+    getConnection(){
+        return new Promise(function(result, reject){
+            pool.getConnection().
+            then(function(conn){
+                result(conn);
+            }).
+            catch(function(error){
+                reject(error);
+            });
+        });
+    }
+};
