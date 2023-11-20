c1536a2cce407e53fce5c679a44674525c2ebb37
nhy42 <26034461+nhy42@users.noreply.github.com>
2023-11-20 15:00:00 +0100

    Prepared server for controllers

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 const pool = require("./utils/db.js");  // to init db
 
 app.set("view engine", "ejs")
+app.set("views", "private/template")
+
 app.use(cookieParser());
 app.use("/css", express.static(__dirname + "/static/css"));
 app.use("/js", express.static(__dirname + "/static/js"));
@@ -13,22 +15,6 @@ app.use("/img", express.static(__dirname + "/static/img"));
 
 app.get("/", (req, res) => {
     res.render(__dirname + "/private/template/home.ejs");
-});
-
-app.get("/products/woman", (req, res) => {
-    res.render(__dirname + "/private/template/products_W.ejs");
-});
-
-app.get("/products/man", (req, res) => {
-    res.render(__dirname + "/private/template/products_M.ejs");
-});
-
-app.get("/products/man/1", (req, res) => {
-    res.render(__dirname + "/private/template/beauty_M.ejs");
-});
-
-app.get("/products/woman/1", (req, res) => {
-    res.render(__dirname + "/private/template/makeup_W.ejs");
 });
 
 app.get("/reviews", (req, res) => {
